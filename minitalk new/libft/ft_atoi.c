7d870a464875e20cca58a61b228c24dc24b4081d
sebferna <116887867+sebasfdezb@users.noreply.github.com>
2024-07-20 21:23:30 +0200

    Add files via upload

---
+++ minitalk new/libft/ft_atoi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 11:33:35 by sebferna          #+#    #+#             */
+/*   Updated: 2024/07/01 11:33:35 by sebferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int				i;
+	long long int	nbr;
+
+	nbr = 0;
+	i = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
+		if (nbr > INT_MAX)
+			return (-1);
+	}
+	return (nbr);
+}
+
