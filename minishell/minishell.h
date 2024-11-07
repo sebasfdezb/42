34577e86a1a589de8815c26e33d7cd748fa89fd9
sebferna <sebasfdezb@gmail.com>
2024-11-07 12:24:53 +0100

    mini

---
+++ minishell/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 11:50:05 by sebferna          #+#    #+#             */
+/*   Updated: 2024/11/07 11:50:05 by sebferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+#endif
+
+
