47428a38c0468252ddc6680aa3ff14bf508631af
sebferna <sebasfdezb@gmail.com>
2024-11-07 18:47:14 +0100

    mini

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 12:48:16 by sebferna          #+#    #+#             */
-/*   Updated: 2024/11/07 18:31:32 by sebferna         ###   ########.fr       */
+/*   Updated: 2024/11/07 18:47:05 by sebferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,12 @@ typedef struct s_data
 {
 	char	*prompt;
 }t_data;
+
+//UTILS
+void	draw(void);
+void    signals(int sig);
+void    sig_ctrlc(int sig);
+void    signals(int sig);
+
 
 #endif
