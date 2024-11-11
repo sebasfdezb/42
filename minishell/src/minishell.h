594289e07c572b3f5d329d984b1b4e056054d19a
sebferna <sebasfdezb@gmail.com>
2024-11-11 19:52:38 +0100

    mini

--- minishell/src/minishell.h
+++ minishell/src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 12:48:16 by sebferna          #+#    #+#             */
-/*   Updated: 2024/11/11 19:04:45 by sebferna         ###   ########.fr       */
+/*   Updated: 2024/11/11 19:52:14 by sebferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,13 @@ typedef	struct s_envp
 
 //UTILS
 void	draw(void);
+
+//EXPORT
+void	set_envp_index(t_data *data);
+int		exp_cmp(const char *s1, const char *s2);
+int		size_envp(t_envp *lst);
+
+//SIGNALS
 void    signals(int sig);
 void    sig_ctrlc(int sig);
 void    signals(int sig);
