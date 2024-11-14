4d3304b5dd4463ba7c6455e162734640138272ff
sebferna <sebasfdezb@gmail.com>
2024-11-14 20:05:35 +0100

    mini

--- minishell/src/mini_utils/parsing/parsing_utils.c
+++ minishell/src/mini_utils/parsing/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 18:19:55 by sebferna          #+#    #+#             */
-/*   Updated: 2024/11/14 19:51:38 by sebferna         ###   ########.fr       */
+/*   Updated: 2024/11/14 20:04:15 by sebferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,17 @@ int	get_token_filein(t_data *data, int *i, int *j, t_parser **node)
 			return (printf("Error: Syntax token `>'\n"), EXIT_FAILURE);
 		if (data->cmd[*i][*j] == '<' && ++(*j))
 			data->flag_hered = 1;
+		if (data->flag_hered == 1 && data->cmd[*i][*j] == '\0')
+			return (printf("Error: Syntax token newline\n"), EXIT_FAILURE);
+		if (data->flag_hered == 1 && data->cmd[*i][*j] == '<')
+			return (printf("Error: Syntax token <<\n"), EXIT_FAILURE);
+		if (data->flag_hered == 1 && data->cmd[*i][*j] == '>')
+			return (printf("Error: Syntax token >>\n"), EXIT_FAILURE);
+		while (data->cmd[*i][*j] == ' ' && ++(*j))
+			if (get_token_next(data, i, j) == EXIT_FAILURE)
+				return (EXIT_FAILURE);
+		if (get_token_next(data, i, j, node) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
 	}
+	return (EXIT_SUCCESS);
 }
