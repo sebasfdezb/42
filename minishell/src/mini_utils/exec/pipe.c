6105075d2ec96a72b9aa96b90241c4fc80e85942
sebferna <sebasfdezb@gmail.com>
2024-12-02 17:50:21 +0100

    mini

--- minishell/src/mini_utils/exec/pipe.c
+++ minishell/src/mini_utils/exec/pipe.c
@@ -6,11 +6,57 @@
 /*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 17:10:18 by sebferna          #+#    #+#             */
-/*   Updated: 2024/12/02 17:24:58 by sebferna         ###   ########.fr       */
+/*   Updated: 2024/12/02 17:48:54 by sebferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+void	handle_child_command(t_data *d, t_parser *n, char **envp, t_list *tmp)
+{
+	if (n->fileout == 1)
+	{
+		if (dup2(d->fd[1], STDOUT_FILENO) == -1)
+			error_msg_pipe("Outfile wrt error");
+	}
+	else if (n->fileout != 1)
+	{
+		if (dup2(n->fileout, STDOUT_FILENO) == -1)
+			error_msg_pipe("Outfile wrt error");
+		close(n->fileout);		
+	}
+	close(d->fd[1]);
+	if (ft_strncmp(n->route, "b\0", 2) == 0)
+	{
+		if (n->fileout == 1 && ((t_parser *)(tmp->next->content))->filein == 0)
+			((t_parser *)(tmp->next->content))->fileout = d->fd[0];
+		ex_builts(d, n);
+		exit(1);
+	}
+	else
+		g_last_status = execve(n->route, n->all_cmd, envp);
+}
+
+void	handle_child_pipe(t_data *d, t_parser *node, char **envp, t_list *tmp)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	if (d->flag_hered == 1)
+	{
+		if (dup2(node->filein, STDIN_FILENO) == -1)
+			error_msg_pipe("Here_doc read error\n");
+		unlink("here_doc.tmp");
+		close(node->filein);
+	}
+	else if (node->filein != STDIN_FILENO)
+	{
+		if (dup2(node->filein, STDIN_FILENO) == -1)
+			error_msg_pipe("Infile read error\n");
+		close(node->filein);
+	}
+	close(d->fd[0]);
+	handle_child_command(d, node, envp, tmp);
+}
 
 void	ex_routepipes(t_data *data, t_parser *node, char **envp, t_list *tmp)
 {
