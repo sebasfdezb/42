3e0bd18533cf2c4c202c9309d0eb6d0c6026aa2e
sebferna <sebasfdezb@gmail.com>
2024-12-02 18:00:25 +0100

    mini

--- minishell/src/mini_utils/exec/error_msg.c
+++ minishell/src/mini_utils/exec/error_msg.c
@@ -6,16 +6,24 @@
 /*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 18:39:29 by sebferna          #+#    #+#             */
-/*   Updated: 2024/11/25 18:41:03 by sebferna         ###   ########.fr       */
+/*   Updated: 2024/12/02 17:51:50 by sebferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void	error_msg(char *str)
+void	error_msg_pipe(char *error)
 {
 	unlink("here_doc.tmp");
 	g_last_status = 1;
-	perror(str);
+	perror(error);
+	exit(EXIT_FAILURE);
+}
+
+void	error_msg(char *error)
+{
+	unlink("here_doc.tmp");
+	g_last_status = 1;
+	perror(error);
 	exit(EXIT_FAILURE);
 }
--- minishell/src/mini_utils/exec/pipe.c
+++ minishell/src/mini_utils/exec/pipe.c
@@ -6,11 +6,48 @@
 /*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 17:10:18 by sebferna          #+#    #+#             */
-/*   Updated: 2024/12/02 17:48:54 by sebferna         ###   ########.fr       */
+/*   Updated: 2024/12/02 18:00:20 by sebferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+void	process_pipe_out(t_parser *node, char **envp)
+{
+	if (node->fileout != STDOUT_FILENO)
+	{
+		if (dup2(node->fileout, STDOUT_FILENO) == -1)
+			error_msg("Last command wrt error\n");
+		close(node->fileout);
+	}
+	g_last_status = execve(node->route, node->all_cmd, envp);
+}
+
+void	process_final_pipe(t_data *data, t_parser *node, char **envp)
+{
+	if (node->filein != 0 && data->flag_hered == 1)
+	{
+		if (dup2(node->filein, STDIN_FILENO) == -1)
+			error_msg_pipe("Here_doc read error\n");
+	}
+	else if (node->filein != 0 && data->flag_hered == 0)
+	{
+		if (dup2(node->filein, STDIN_FILENO) == -1)
+			error_msg_pipe("Infile read error\n");
+	}
+	else if (data->fd[1] == STDOUT_FILENO)
+	{
+		if (dup2(data->fd[0], STDIN_FILENO) == -1)
+			error_msg_pipe("Read error\n");
+	}
+	else if (node->filein == 0)
+	{
+		if (dup2(node->filein, STDIN_FILENO) == -1)
+			error_msg_pipe("Infile read error\n");
+	}
+	close(node->filein);
+	process_pipe_out(node, envp);
+}
 
 void	handle_child_command(t_data *d, t_parser *n, char **envp, t_list *tmp)
 {
@@ -23,7 +60,7 @@ void	handle_child_command(t_data *d, t_parser *n, char **envp, t_list *tmp)
 	{
 		if (dup2(n->fileout, STDOUT_FILENO) == -1)
 			error_msg_pipe("Outfile wrt error");
-		close(n->fileout);		
+		close(n->fileout);
 	}
 	close(d->fd[1]);
 	if (ft_strncmp(n->route, "b\0", 2) == 0)
