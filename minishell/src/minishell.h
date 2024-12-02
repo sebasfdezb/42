482fead0376eb8d7a3d9a2aa6cf3c3240d6182de
sebferna <sebasfdezb@gmail.com>
2024-12-02 18:12:28 +0100

    mini

--- minishell/src/minishell.h
+++ minishell/src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sebferna <sebferna@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 12:48:16 by sebferna          #+#    #+#             */
-/*   Updated: 2024/12/02 17:14:23 by sebferna         ###   ########.fr       */
+/*   Updated: 2024/12/02 18:12:20 by sebferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,13 +85,37 @@ void	signals(int sig);
 void	sig_ctrlc(int sig);
 void	sig_ctrlslash(int sig);
 //free
-void	free_split(char **tmp);
+void		free_split(char **tmp);
+void		free_cd(t_data *data);
+void		free_node(t_list **lst);
+static void	free_command(t_list **lst);
 //splits
 int		get_path(t_data *d);
 int		get_cmds(t_data *d);
+char	**get_words(t_data *data, char const *str, char c, int i);
 //lexer
 int		lexer(char	*str);
+int		check_builts(t_parser *node);
+int		check_path(t_data *data);
 //expander
 void	expand(t_data *d);
-
+//parsing
+int	parsing(t_data *data, int i, int j);
+int	get_token_filein(t_data *data, int *i, int *j, t_parser **node);
+int	get_token_out(t_data *data, int *i, int *j, t_parser **node);
+void	ft_heredoc(t_data *data, t_parser **node, char *str);
+//route
+int	process_route(t_data *data, int i);
+//exec
+int	execute(t_data *data, t_parser *node, char **envp, t_list *aux);
+void	ex_routepipes(t_data *data, t_parser *node, char **envp, t_list *tmp);
+void	error_msg_pipe(char *error);
+void	error_msg(char *error);
+//builts
+void	ex_cd(t_data *data, char **str, int flag);
+void	ex_echo(char **str, int flag, int fd);
+void	get_envp(t_data *data, char **envp, int i);
+int	ex_exit(t_data *data, t_parser *node, int i, int fd);
+void	ex_pwd(int fd);
+void	ex_unset(t_data *data, char *str);
 #endif
